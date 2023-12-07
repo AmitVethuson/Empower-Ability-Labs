eb9f8cdf8cb79c46337c16000535e7d5af642413
Andrew <han00116@algonquinlive.com>
2023-12-07 13:13:50 -0500

    added lightbox/modal

--- js/EmpowerAbilityLab.js
+++ js/EmpowerAbilityLab.js
@@ -97,6 +97,21 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
+function openModal() {
+    document.getElementById('myModal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+
+window.onclick = function(event) {
+    var modal = document.getElementById('myModal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+}
 
 
 
